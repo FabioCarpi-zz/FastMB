3c1af15f1dbae73a6d1a02e6db5bb821c7338560
Fabio <fabiocarpi@hotmail.com.br>
2017-01-09 18:29:53 -0200

    Versão 1.2.49
    chart.php
    - Alterado o tipo de gráfico

--- src/www/chart.php
+++ src/www/chart.php
@@ -1,38 +1,37 @@
 <?php require_once("system.php");?>
 
 <html>
-<head>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script type="text/javascript">
-		google.charts.load('current', {'packages':['line']});
-		google.charts.setOnLoadCallback(drawChart);
-		function drawChart() {
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'Hora');
-			data.addColumn('number', 'Valor');
-			data.addRows([<?php
+	<head>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type='text/javascript'>
+			google.charts.load('current', {'packages':['annotationchart']});
+			google.charts.setOnLoadCallback(drawChart);
+			function drawChart(){
+				var data = google.visualization.arrayToDataTable([
+				['Hora','Valor'],<?php
 				$dados = file_get_contents("https://www.mercadobitcoin.net/api/trades/");
 				$dados = json_decode($dados, true);
-				$dados = array_reverse($dados);
 				foreach($dados as $dado){
-					echo "['".date("H:i:s",$dado["date"])."',".$dado["price"]."],";
+					echo "[new Date(".
+					date("Y", $dado["date"]).",".
+					date("m", $dado["date"]).",".
+					date("d", $dado["date"]).",".
+					date("H", $dado["date"]).",".
+					date("i", $dado["date"]).",".
+					date("s", $dado["date"])."),".$dado["price"]."],";
 				}?>
-			]);
-			var formatter = new google.visualization.NumberFormat(
-				{negativeColor: 'red', negativeParens: true, pattern: '$#,###.#####'}
-			);
-			formatter.format(data, 1);
-			var options = {
-				legend: {position: 'none'},
-				width: 600,
-				height: 600
-			};
-			var chart = new google.charts.Line(document.getElementById('line_top_x'));
-			chart.draw(data, options);
-		}
-	</script>
-</head>
-<body>
-	<div id="line_top_x"></div>
-</body>
+				]);
+				var chart = new google.visualization.AnnotationChart(document.getElementById('chart_div'));
+				var options = {
+					displayAnnotations: true
+				};
+				chart.draw(data, options);
+			}
+		</script>
+	</head>
+	<body>
+		<div id='chart_div' style='width: 610px; height: 620px;'></div>
+		<br>
+		<center><a href="#" onclick="location.reload()">Atualizar</a></center>
+	</body>
 </html>
